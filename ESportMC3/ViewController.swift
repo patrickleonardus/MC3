40bbcc929fc6bceb23b23483b71f4c1d4d112a58
Patrick Leonardus <patrickleonardus@gmail.com>
2019-08-20 13:57:14 +0700

    Initial Commit

---
+++ ESportMC3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ESportMC3
+//
+//  Created by Patrick Leonardus on 20/08/19.
+//  Copyright © 2019 Patrick Leonardus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
